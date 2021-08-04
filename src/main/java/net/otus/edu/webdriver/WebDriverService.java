97335260e18370bbc6e7528d0427688043175753
Eugene <e.venediktov@bi.zone>
2021-08-04 18:11:07 +0300

    Добавлена многопоточность и распараллеливание junit тестов. Удалена реализация WebHooks

---
+++ src/main/java/net/otus/edu/webdriver/WebDriverService.java
@@ -0,0 +1,53 @@
+package net.otus.edu.webdriver;
+
+import com.epam.healenium.SelfHealingDriver;
+import net.otus.edu.enums.Browsers;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
+public class WebDriverService {
+    private WebDriverService() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    private static final Logger LOGGER = LogManager.getLogger(WebDriverService.class);
+    private static final ThreadLocal<WebDriver> DRIVER = new ThreadLocal<>();
+
+    public static void setDriver() {
+        LOGGER.info("Инициализация драйвера и создание сессии");
+        DRIVER.set(initSelfHealingDriver());
+    }
+
+    public static WebDriver getDriver() {
+        return DRIVER.get();
+    }
+
+    public static void closeDriver() {
+        LOGGER.info("Завершение сессии драйвера");
+        DRIVER.get().close();
+        DRIVER.remove();
+    }
+
+    protected static WebDriver initDriver() {
+        String browserName = System.getProperty("browser");
+        WebDriver driver;
+        if (browserName != null && Browsers.containValue(browserName)) {
+            driver = WebDriverFactory.create(Browsers.valueOf(browserName.toUpperCase(Locale.ROOT)));
+        } else {
+            driver = WebDriverFactory.create(Browsers.CHROME);
+        }
+        if (driver != null) {
+            driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
+            driver.manage().window().maximize();
+        }
+        return driver;
+    }
+
+    protected static SelfHealingDriver initSelfHealingDriver() {
+        return SelfHealingDriver.create(initDriver());
+    }
+}
--- src/test/java/net/otus/edu/epam/EventApplicationTests.java
+++ src/test/java/net/otus/edu/epam/EventApplicationTests.java
@@ -1,14 +1,27 @@
 package net.otus.edu.epam;
 
-import net.otus.edu.WebHooks;
 import net.otus.edu.pages.epam.events.EventPage;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class EventApplicationTests extends WebHooks {
+import static net.otus.edu.webdriver.WebDriverService.*;
+
+class EventApplicationTests {
     private static final Logger LOGGER = LogManager.getLogger(EventApplicationTests.class);
+
+    @BeforeEach
+    public void setUp() {
+        setDriver();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        closeDriver();
+    }
 
     @Test
     void viewUpcomingEvents() {
@@ -19,6 +32,20 @@ class EventApplicationTests extends WebHooks {
         LOGGER.info("Текущий показатель счетчика вкладки {}: {}", tabName, currentCounterValue);
         Integer eventCardsCount = eventPage.getEventCards().size();
         LOGGER.info("Текущие кол-во карточек с событиями: {}", eventCardsCount);
-        Assertions.assertEquals(eventCardsCount, currentCounterValue);
+        Assertions.assertEquals(eventCardsCount, currentCounterValue,
+                "текущее значение счетчика не соответствует реальному кол-ву событий");
+    }
+
+    @Test
+    void viewUpcomingEvents2() {
+        EventPage eventPage = new EventPage(getDriver()).open();
+        LOGGER.info("Переход на страницу: {}", eventPage.getTitle());
+        String tabName = "Upcoming events";
+        Integer currentCounterValue = eventPage.clickTabByName(tabName).getTabCounter();
+        LOGGER.info("Текущий показатель счетчика вкладки {}: {}", tabName, currentCounterValue);
+        Integer eventCardsCount = eventPage.getEventCards().size();
+        LOGGER.info("Текущие кол-во карточек с событиями: {}", eventCardsCount);
+        Assertions.assertEquals(eventCardsCount, currentCounterValue,
+                "текущее значение счетчика не соответствует реальному кол-ву событий");
     }
 }
